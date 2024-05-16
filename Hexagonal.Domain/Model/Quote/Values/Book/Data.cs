46a13e460864044a21b38c5f007391b58e0852d4
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-16 18:19:48 -0500

    Create the value objects for the aggregate

---
+++ Hexagonal.Domain/Model/Quote/Values/Book/Data.cs
@@ -0,0 +1,34 @@
+﻿using LaPinguinera.Domain.Generic;
+using LaPinguinera.Quotes.Domain.Model.Quote.Values.Book.Enums;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote.Values.Book;
+
+public class Data : IValueObject<(string Title, string Author, decimal SellPrice, BookType Type)>
+{
+    public (string Title, string Author, decimal SellPrice, BookType Type) Value { get; private set; }
+
+    private Data((string? Title, string? Author, decimal Price, BookType Type) value)
+    {
+        if (string.IsNullOrWhiteSpace(value.Title))
+        {
+            throw new ArgumentException("Title cannot be null or empty");
+        }
+
+        if (string.IsNullOrWhiteSpace(value.Author))
+        {
+            throw new ArgumentException("Author cannot be null or empty");
+        }
+
+        if (value.Price <= 0)
+        {
+            throw new ArgumentException("Price cannot be less than or equal to zero");
+        }
+
+        Value = value!;
+    }
+
+    public static Data Of(string? Title, string? Author, decimal Price, BookType Type)
+    {
+        return new Data((Title, Author, Price, Type));
+    }
+}
---
+++ Hexagonal.Domain/Model/Quote/Values/Root/RestBudget.cs
@@ -0,0 +1,23 @@
+﻿using LaPinguinera.Domain.Generic;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote.Values.Root;
+
+public class RestBudget : IValueObject<decimal>
+{
+	public decimal Value { get; private set; }
+
+	private RestBudget( decimal value )
+	{
+		if (value < 0)
+		{
+			throw new ArgumentException( "Rest budget cannot be less than zero" );
+		}
+
+		Value = value;
+	}
+
+	public static RestBudget Of( decimal value )
+	{
+		return new RestBudget( value );
+	}
+}

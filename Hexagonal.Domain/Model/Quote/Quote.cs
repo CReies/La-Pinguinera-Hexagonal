7a1add6b43a251bbadffaf52b4e6356fb3c8b6ea
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-16 18:22:15 -0500

    Add the aggregate root with its behavior and the domain event to create a book

---
+++ Hexagonal.Domain/Model/Quote/Quote.cs
@@ -0,0 +1,24 @@
+﻿using LaPinguinera.Domain.Generic;
+using LaPinguinera.Quotes.Domain.Model.Quote.Entities;
+using LaPinguinera.Quotes.Domain.Model.Quote.Events;
+using LaPinguinera.Quotes.Domain.Model.Quote.Values.Book.Enums;
+using LaPinguinera.Quotes.Domain.Model.Quote.Values.Root;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote;
+
+public class Quote : AggregateRoot<QuoteId>
+{
+	public List<AbstractBook> Inventory { get; set; } = [];
+	public (List<(List<AbstractBook> QuoteGroup, TotalPrice TotalPrice, Discount Discount)> Quote, TotalPrice TotalPrice, Discount Discount) Result { get; set; }
+	public RestBudget? RestBudget { get; set; }
+
+	public Quote( QuoteId id ) : base( id )
+	{
+		Subscribe( new QuoteBehavior( this ) );
+	}
+
+	public void CalculateIndividualBook( string? title, string? author, decimal basePrice, BookType bookType )
+	{
+		AppendEvent( new IndividualPriceCalculated( title, author, basePrice, bookType ) ).Invoke();
+	}
+}
---
+++ Hexagonal.Domain/Model/Quote/QuoteBehavior.cs
@@ -0,0 +1,25 @@
+﻿using LaPinguinera.Domain.Generic;
+using LaPinguinera.Quotes.Domain.Model.Quote.Entities;
+using LaPinguinera.Quotes.Domain.Model.Quote.Events;
+using LaPinguinera.Quotes.Domain.Model.Quote.Factory;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote;
+
+public class QuoteBehavior : Behavior
+{
+	public QuoteBehavior( Quote quote )
+	{
+		AddCalculateIndividualSub( quote );
+	}
+
+	private void AddCalculateIndividualSub( Quote quote )
+	{
+		AddSub( ( IndividualPriceCalculated domainEvent ) =>
+		{
+			BookFactory _bookFactory = new();
+			var book = _bookFactory.Create( domainEvent.Title, domainEvent.Author, domainEvent.BasePrice, domainEvent.BookType );
+			book.CalculateSellPrice();
+			quote.Inventory.Add( book );
+		} );
+	}
+}

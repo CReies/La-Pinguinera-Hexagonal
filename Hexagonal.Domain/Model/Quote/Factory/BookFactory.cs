56fc1777edb4bcfa466a01b62bdcaaba5bf5a1f3
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-16 18:21:44 -0500

    Add a factory to create the books

---
+++ Hexagonal.Domain/Model/Quote/Factory/BookFactory.cs
@@ -0,0 +1,20 @@
+﻿using LaPinguinera.Quotes.Domain.Model.Quote.Entities;
+using LaPinguinera.Quotes.Domain.Model.Quote.Values.Book.Enums;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote.Factory;
+
+public class BookFactory
+{
+	public AbstractBook Create( string? title, string? author, decimal basePrice, BookType type )
+	{
+		var bookChildren = new Dictionary<BookType, AbstractBook>
+		{
+			{BookType.BOOK, Book.From(title, author, basePrice) },
+			{BookType.NOVEL, Novel.From(title, author, basePrice) }
+		};
+
+		var book = bookChildren[type];
+
+		return book ?? throw new ArgumentException( "Book type not found" );
+	}
+}

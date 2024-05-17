cce8a159ad32d63a5005670aba3efa6f6bd397da
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-16 21:15:18 -0500

    Add CustomerId, CustomerSeniority, and RegisterDate value objects

---
+++ Hexagonal.Domain/Model/Quote/Values/Customer/RegisterDate.cs
@@ -0,0 +1,18 @@
+﻿using LaPinguinera.Domain.Generic;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote.Values.Customer;
+
+public class RegisterDate : IValueObject<DateOnly>
+{
+	public DateOnly Value { get; }
+
+	private RegisterDate( DateOnly value )
+	{
+		Value = value;
+	}
+
+	public static RegisterDate Of( DateOnly value )
+	{
+		return new RegisterDate( value );
+	}
+}

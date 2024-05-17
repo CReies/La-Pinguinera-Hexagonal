56d209bdb105d9785f8de80602c2091a57aa43ee
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-17 06:42:49 -0500

    Add GroupPriceCalculated domain event

--- Hexagonal.Domain/Model/Quote/Events/Enums/EventType.cs
+++ Hexagonal.Domain/Model/Quote/Events/Enums/EventType.cs
@@ -5,5 +5,6 @@ public enum EventType
 	IndividualPriceCalculated,
 	ListPriceCalculated,
 	QuoteCreated,
-	BudgetCalculated
+	BudgetCalculated,
+	GroupPriceCalculated,
 }
---
+++ Hexagonal.Domain/Model/Quote/Events/GroupPriceCalculated.cs
@@ -0,0 +1,10 @@
+﻿using LaPinguinera.Domain.Generic;
+using LaPinguinera.Quotes.Domain.Model.Quote.Events.Enums;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote.Events;
+
+public class GroupPriceCalculated( List<List<(string bookId, int quantity)>> groupsRequested, DateOnly customerRegisterDate ) : DomainEvent( EventType.GroupPriceCalculated.ToString() )
+{
+	public List<List<(string bookId, int quantity)>> GroupsRequested { get; set; } = groupsRequested;
+	public DateOnly CustomerRegisterDate { get; set; } = customerRegisterDate;
+}

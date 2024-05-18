6c836c2eb2d9e7a1934a54b550f147d4036d7d99
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-17 22:46:36 -0500

    Add commands for every use case

---
+++ Hexagonal.Domain/Model/Quote/Commands/CalculateBudgetCommand.cs
@@ -0,0 +1,14 @@
+﻿using LaPinguinera.Domain.Generic;
+using LaPinguinera.Quotes.Domain.Model.Quote.Values.Root;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote.Commands;
+
+public class CalculateBudgetCommand(
+	string quoteId,
+	List<string> bookIds,
+	DateOnly customerRegisterDate
+) : Command<QuoteId>( QuoteId.Of( quoteId ) )
+{
+	public List<string> BookIds { get; } = bookIds;
+	public DateOnly CustomerRegisterDate { get; } = customerRegisterDate;
+}
---
+++ Hexagonal.Domain/Model/Quote/Commands/CalculateGroupCommand.cs
@@ -0,0 +1,14 @@
+﻿using LaPinguinera.Domain.Generic;
+using LaPinguinera.Quotes.Domain.Model.Quote.Values.Root;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote.Commands;
+
+public class CalculateGroups(
+	string quoteId,
+	List<List<(string bookId, int bookQuantity)>> bookGroups,
+	DateOnly customerRegisterDate
+) : Command<QuoteId>( QuoteId.Of( quoteId ) )
+{
+	public List<List<(string bookId, int bookQuantity)>> BookGroups { get; } = bookGroups;
+	public DateOnly CustomerRegisterDate { get; } = customerRegisterDate;
+}

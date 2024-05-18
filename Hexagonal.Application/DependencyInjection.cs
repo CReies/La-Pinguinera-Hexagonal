6f3634892f75ec374d40444dd818605d1f35fa7e
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-18 17:01:22 -0500

    Refactor DependencyInjection.cs to add use cases for creating and calculating quotes

--- Hexagonal.Application/DependencyInjection.cs
+++ Hexagonal.Application/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using LaPinguinera.Application.Generic;
+using LaPinguinera.Quotes.Application.UseCases;
+using LaPinguinera.Quotes.Domain.Model.Quote.Commands;
+using LaPinguinera.Quotes.Domain.Model.Quote.Values.Root;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LaPinguinera.Application;
 
@@ -6,6 +10,12 @@ public static class DependencyInjection
 {
 	public static IServiceCollection AddApplication( this IServiceCollection services )
 	{
+		services.AddTransient<IInitialCommandUseCase<CreateQuoteCommand>, CreateQuoteUseCase>();
+		services.AddTransient<ICommandUseCase<CalculateIndividualCommand, QuoteId>, CalculateIndividualUseCase>();
+		services.AddTransient<ICommandUseCase<CalculateListCommand, QuoteId>, CalculateListUseCase>();
+		services.AddTransient<ICommandUseCase<CalculateBudgetCommand, QuoteId>, CalculateBudgetUseCase>();
+		services.AddTransient<ICommandUseCase<CalculateGroupCommand, QuoteId>, CalculateGroupUseCase>();
+
 		return services;
 	}
 }

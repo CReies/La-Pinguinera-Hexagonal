87f64c6e8cd01d98e7ede8c530c376502bd4ae51
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-19 06:30:44 -0500

    Refactor BookFactory to handle book creation with or without an ID

--- Hexagonal.Domain/Model/Quote/Factory/BookFactory.cs
+++ Hexagonal.Domain/Model/Quote/Factory/BookFactory.cs
@@ -5,16 +5,29 @@ namespace LaPinguinera.Quotes.Domain.Model.Quote.Factory;
 
 public class BookFactory
 {
-	public AbstractBook Create(string? title, string? author, decimal basePrice, BookType type)
+	public AbstractBook Create( string? id, string? title, string? author, decimal basePrice, BookType type )
 	{
-		var bookChildren = new Dictionary<BookType, AbstractBook>
+		Dictionary<BookType, AbstractBook> bookChildren;
+
+		if (id is null)
 		{
-			{BookType.BOOK, Book.From(title, author, basePrice) },
-			{BookType.NOVEL, Novel.From(title, author, basePrice) }
-		};
+			bookChildren = new Dictionary<BookType, AbstractBook>
+			{
+				{BookType.BOOK, Book.From(title, author, basePrice) },
+				{BookType.NOVEL, Novel.From(title, author, basePrice) }
+			};
+		}
+		else
+		{
+			bookChildren = new Dictionary<BookType, AbstractBook>
+			{
+				{BookType.BOOK, Book.From(id, title, author, basePrice) },
+				{BookType.NOVEL, Novel.From(id, title, author, basePrice) }
+			};
+		}
 
 		var book = bookChildren[type];
 
-		return book ?? throw new ArgumentException("Book type not found");
+		return book ?? throw new ArgumentException( "Book type not found" );
 	}
 }

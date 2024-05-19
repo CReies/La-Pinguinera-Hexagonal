3c69f3d5af56846b046bd1e9fb2673a6e0d4fb95
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-19 06:32:24 -0500

    Add endpoints for calculating budget and list requests

--- Hexagonal.Api/Controllers/QuoteController.cs
+++ Hexagonal.Api/Controllers/QuoteController.cs
@@ -40,4 +40,60 @@ public class QuoteController : ControllerBase
 			return BadRequest( ex.Message );
 		}
 	}
+
+	[HttpPost( "list" )]
+	public async Task<IActionResult> CalculateList
+	(
+		[FromBody] CalculateListReqDTO request,
+		[FromServices] ICommandUseCase<CalculateListCommand, QuoteId, CalculateListResDTO> useCaseCommand
+	)
+	{
+		try
+		{
+			var bookIdWithQuantity = request.Books
+				.Select( b => (bookId: b.BookId, quantity: b.Quantity) )
+				.ToList();
+			var command = new CalculateListCommand(
+				request.AggregateId,
+				bookIdWithQuantity,
+				request.CustomerRegisterDate
+			);
+			var subject = Observable.Return( command );
+			var resultObservable = useCaseCommand.Execute( subject );
+			var result = await resultObservable.FirstAsync();
+
+			return Ok( result );
+		}
+		catch (Exception ex)
+		{
+			return BadRequest( ex.Message );
+		}
+	}
+
+	[HttpPost( "budget" )]
+	public async Task<IActionResult> CalculateBudget
+	(
+		[FromBody] CalculateBudgetReqDTO request,
+		[FromServices] ICommandUseCase<CalculateBudgetCommand, QuoteId, CalculateBudgetResDTO> useCaseCommand
+	)
+	{
+		try
+		{
+			var command = new CalculateBudgetCommand(
+				request.AggregateId,
+				request.BookIds,
+				request.Budget,
+				request.CustomerRegisterDate
+			);
+			var subject = Observable.Return( command );
+			var resultObservable = useCaseCommand.Execute( subject );
+			var result = await resultObservable.FirstAsync();
+
+			return Ok( result );
+		}
+		catch (Exception ex)
+		{
+			return BadRequest( ex.Message );
+		}
+	}
 }

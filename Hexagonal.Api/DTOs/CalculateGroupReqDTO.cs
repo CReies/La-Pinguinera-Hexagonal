ef4bebf1b97c3b4674573bcc5a249ac52c054ed1
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-19 18:54:34 -0500

    Add CalculateGroupReqDTO for calculating group requests

---
+++ Hexagonal.Api/DTOs/CalculateGroupReqDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace LaPinguinera.Quotes.Infrastructure.Api.DTOs;
+
+public class CalculateGroupReqDTO
+{
+	public string AggregateId { get; set; }
+	public List<List<BookIdWithQuantityDTO>> Group { get; set; }
+	public DateOnly CustomerRegisterDate { get; set; }
+}

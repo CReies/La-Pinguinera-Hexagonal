b8574dfa06dd9c035883c8d9a5685438f7dd3b04
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-18 23:14:48 -0500

    Add persistence layer for quotes

--- Hexagonal.Api/Program.cs
+++ Hexagonal.Api/Program.cs
@@ -1,4 +1,5 @@
 using LaPinguinera.Application;
+using LaPinguinera.Quotes.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder( args );
 
@@ -6,6 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
+builder.Services.AddPersistence( builder.Configuration );
 
 var app = builder.Build();
 

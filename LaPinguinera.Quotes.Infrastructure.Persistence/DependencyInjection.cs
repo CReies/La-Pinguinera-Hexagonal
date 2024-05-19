b529f8b2c701da86cd3bafd98af0a3827562bd4b
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-19 06:28:34 -0500

    Refactor DependencyInjection class in Persistence module

--- LaPinguinera.Quotes.Infrastructure.Persistence/DependencyInjection.cs
+++ LaPinguinera.Quotes.Infrastructure.Persistence/DependencyInjection.cs
@@ -1,22 +1,22 @@
-﻿using LaPinguinera.Application.Generic;
-using LaPinguinera.Domain.Generic;
+﻿using Hexagonal.Library.Quotes.Infrastructure.Persistence;
+using LaPinguinera.Application.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
 
 
-namespace LaPinguinera.Quotes.Infrastructure.Persistence;
+namespace LaPinguinera.Infrastructure.Persistence;
 
 public static class DependencyInjection
 {
 	public static IServiceCollection AddPersistence( this IServiceCollection services, IConfiguration configuration )
 	{
 
-		services.AddScoped<IMongoCollection<DomainEvent>>( ( options ) =>
+		services.AddScoped( ( options ) =>
 		{
 			var client = new MongoClient( configuration["Database:ConnectionString"] );
 			var database = client.GetDatabase( configuration["Database:DatabaseName"] );
-			return database.GetCollection<DomainEvent>( configuration["Database:CollectionName"] );
+			return database.GetCollection<Event>( configuration["Database:CollectionName"] );
 		} );
 
 		services.AddScoped<IEventsRepository, EventsRepository>();

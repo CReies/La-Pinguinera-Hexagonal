4bcc01a35d4a996340f169254e4584fa0fbcd90a
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-18 23:22:29 -0500

    feat: Add response DTOs and mappers for quote use cases

--- Hexagonal.Application/DependencyInjection.cs
+++ Hexagonal.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using LaPinguinera.Application.Generic;
+using LaPinguinera.Quotes.Application.DTOs;
 using LaPinguinera.Quotes.Application.UseCases;
 using LaPinguinera.Quotes.Domain.Model.Quote.Commands;
 using LaPinguinera.Quotes.Domain.Model.Quote.Values.Root;
@@ -10,11 +11,11 @@ public static class DependencyInjection
 {
 	public static IServiceCollection AddApplication( this IServiceCollection services )
 	{
-		services.AddTransient<IInitialCommandUseCase<CreateQuoteCommand>, CreateQuoteUseCase>();
-		services.AddTransient<ICommandUseCase<CalculateIndividualCommand, QuoteId>, CalculateIndividualUseCase>();
-		services.AddTransient<ICommandUseCase<CalculateListCommand, QuoteId>, CalculateListUseCase>();
-		services.AddTransient<ICommandUseCase<CalculateBudgetCommand, QuoteId>, CalculateBudgetUseCase>();
-		services.AddTransient<ICommandUseCase<CalculateGroupCommand, QuoteId>, CalculateGroupUseCase>();
+		services.AddTransient<IInitialCommandUseCase<CreateQuoteCommand, CreateQuoteResDTO>, CreateQuoteUseCase>();
+		services.AddTransient<ICommandUseCase<CalculateIndividualCommand, QuoteId, CalculateIndividualResDTO>, CalculateIndividualUseCase>();
+		services.AddTransient<ICommandUseCase<CalculateListCommand, QuoteId, CalculateListResDTO>, CalculateListUseCase>();
+		services.AddTransient<ICommandUseCase<CalculateBudgetCommand, QuoteId, CalculateBudgetResDTO>, CalculateBudgetUseCase>();
+		services.AddTransient<ICommandUseCase<CalculateGroupCommand, QuoteId, CalculateGroupResDTO>, CalculateGroupUseCase>();
 
 		return services;
 	}

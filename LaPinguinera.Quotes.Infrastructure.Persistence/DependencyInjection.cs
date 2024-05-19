adf45179a5ec4267f141da51fb7fa13a0e389441
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-18 23:09:01 -0500

    Refactor DependencyInjection.cs to use consistent naming and update database configuration

--- LaPinguinera.Quotes.Infrastructure.Persistence/DependencyInjection.cs
+++ LaPinguinera.Quotes.Infrastructure.Persistence/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using LaPinguinera.Application.Generic;
+using LaPinguinera.Domain.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
@@ -11,11 +12,11 @@ public static class DependencyInjection
 	public static IServiceCollection AddPersistence( this IServiceCollection services, IConfiguration configuration )
 	{
 
-		services.AddScoped<IMongoCollection<Event>>( ( options ) =>
+		services.AddScoped<IMongoCollection<DomainEvent>>( ( options ) =>
 		{
-			var client = new MongoClient( configuration["OnlineShopDatabase:ConnectionString"] );
-			var database = client.GetDatabase( configuration["OnlineShopDatabase:DatabaseName"] );
-			return database.GetCollection<Event>( configuration["OnlineShopDatabase:UsersCollectionName"] );
+			var client = new MongoClient( configuration["Database:ConnectionString"] );
+			var database = client.GetDatabase( configuration["Database:DatabaseName"] );
+			return database.GetCollection<DomainEvent>( configuration["Database:CollectionName"] );
 		} );
 
 		services.AddScoped<IEventsRepository, EventsRepository>();

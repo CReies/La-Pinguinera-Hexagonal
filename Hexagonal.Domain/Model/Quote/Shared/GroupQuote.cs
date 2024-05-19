7e5c88c0a8c8b6538b0ab09bbaa568e8188d9a87
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-18 23:11:17 -0500

    Update GroupQuote and Result classes

--- Hexagonal.Domain/Model/Quote/Shared/GroupQuote.cs
+++ Hexagonal.Domain/Model/Quote/Shared/GroupQuote.cs
@@ -3,9 +3,9 @@ using LaPinguinera.Quotes.Domain.Model.Quote.Interfaces;
 
 namespace LaPinguinera.Quotes.Domain.Model.Quote.Shared;
 
-public class GroupQuote : IGroupQuote
+public class GroupQuote() : IGroupQuote
 {
 	public List<AbstractBook> Books { get; set; } = [];
-	public decimal TotalPrice { get; set; }
-	public decimal TotalDiscount { get; set; }
+	public decimal TotalPrice { get; set; } = 0;
+	public decimal TotalDiscount { get; set; } = 0;
 }

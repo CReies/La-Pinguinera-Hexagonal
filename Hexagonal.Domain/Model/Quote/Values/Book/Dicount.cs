fde042bb086adb492ec071b8a327492bbb810247
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-19 20:57:34 -0500

    Add Discount value object for book quotes

---
+++ Hexagonal.Domain/Model/Quote/Values/Book/Dicount.cs
@@ -0,0 +1,23 @@
+﻿using LaPinguinera.Domain.Generic;
+
+namespace LaPinguinera.Quotes.Domain.Model.Quote.Values.Book;
+
+public class Discount : IValueObject<decimal>
+{
+	public decimal Value { get; private set; }
+
+	private Discount( decimal value )
+	{
+		if (value < 0)
+		{
+			throw new ArgumentException( "Final price cannot be less than zero" );
+		}
+
+		Value = value;
+	}
+
+	public static Discount Of( decimal value )
+	{
+		return new Discount( value );
+	}
+}

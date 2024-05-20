d7a4e6729793fdc1aa6d3e4d5269918127f41aa7
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-20 03:05:02 -0500

    Rename controllers

---
+++ Hexagonal.Api/Controllers/CalculateQuoteController.cs
@@ -0,0 +1,129 @@
+﻿using LaPinguinera.Quotes.Application.DTOs;
+using LaPinguinera.Quotes.Application.Generic;
+using LaPinguinera.Quotes.Domain.Model.Quote.Commands;
+using LaPinguinera.Quotes.Domain.Model.Quote.Values.Root;
+using LaPinguinera.Quotes.Infrastructure.Api.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Reactive.Linq;
+
+namespace LaPinguinera.Quotes.Infrastructure.Api.Controllers;
+
+[ApiController]
+[Route( "api/v1/quote" )]
+public class CalculateQuoteController : ControllerBase
+{
+	[HttpPost( "individual" )]
+	public async Task<IActionResult> CalculateIndividual
+	(
+		[FromBody] CalculateIndividualReqDTO request,
+		[FromServices] ICommandUseCase<CalculateIndividualCommand, QuoteId, CalculateIndividualResDTO> useCaseCommand
+	)
+	{
+		try
+		{
+			CalculateIndividualCommand command = new(
+				request.AggregateId,
+				request.Title,
+				request.Author,
+				request.Price,
+				request.Type
+			);
+			IObservable<CalculateIndividualCommand> subject = Observable.Return( command );
+			IObservable<CalculateIndividualResDTO> resultObservable = useCaseCommand.Execute( subject );
+			CalculateIndividualResDTO result = await resultObservable.FirstAsync();
+
+			return Ok( result );
+		}
+		catch (Exception ex)
+		{
+			return BadRequest( ex.Message );
+		}
+	}
+
+	[HttpPost( "list" )]
+	public async Task<IActionResult> CalculateList
+	(
+		[FromBody] CalculateListReqDTO request,
+		[FromServices] ICommandUseCase<CalculateListCommand, QuoteId, CalculateListResDTO> useCaseCommand
+	)
+	{
+		try
+		{
+			List<(string bookId, int quantity)> bookIdWithQuantity = request.Books
+				.Select( b => (bookId: b.BookId, quantity: b.Quantity) )
+				.ToList();
+			CalculateListCommand command = new(
+				request.AggregateId,
+				bookIdWithQuantity,
+				request.CustomerRegisterDate
+			);
+			IObservable<CalculateListCommand> subject = Observable.Return( command );
+			IObservable<CalculateListResDTO> resultObservable = useCaseCommand.Execute( subject );
+			CalculateListResDTO result = await resultObservable.FirstAsync();
+
+			return Ok( result );
+		}
+		catch (Exception ex)
+		{
+			return BadRequest( ex.Message );
+		}
+	}
+
+	[HttpPost( "budget" )]
+	public async Task<IActionResult> CalculateBudget
+	(
+		[FromBody] CalculateBudgetReqDTO request,
+		[FromServices] ICommandUseCase<CalculateBudgetCommand, QuoteId, CalculateBudgetResDTO> useCaseCommand
+	)
+	{
+		try
+		{
+			CalculateBudgetCommand command = new(
+				request.AggregateId,
+				request.BookIds,
+				request.Budget,
+				request.CustomerRegisterDate
+			);
+			IObservable<CalculateBudgetCommand> subject = Observable.Return( command );
+			IObservable<CalculateBudgetResDTO> resultObservable = useCaseCommand.Execute( subject );
+			CalculateBudgetResDTO result = await resultObservable.FirstAsync();
+
+			return Ok( result );
+		}
+		catch (Exception ex)
+		{
+			return BadRequest( ex.Message );
+		}
+	}
+
+	[HttpPost( "group" )]
+	public async Task<IActionResult> CalculateGroup
+(
+	[FromBody] CalculateGroupReqDTO request,
+	[FromServices] ICommandUseCase<CalculateGroupCommand, QuoteId, CalculateGroupResDTO> useCaseCommand
+)
+	{
+		try
+		{
+			List<List<(string bookId, int quantity)>> bookIdWithQuantityGroup = request.Group
+				.Select( g => g
+					.Select( b => (bookId: b.BookId, quantity: b.Quantity) )
+					.ToList() )
+				.ToList();
+			CalculateGroupCommand command = new(
+				request.AggregateId,
+				bookIdWithQuantityGroup,
+				request.CustomerRegisterDate
+			);
+			IObservable<CalculateGroupCommand> subject = Observable.Return( command );
+			IObservable<CalculateGroupResDTO> resultObservable = useCaseCommand.Execute( subject );
+			CalculateGroupResDTO result = await resultObservable.FirstAsync();
+
+			return Ok( result );
+		}
+		catch (Exception ex)
+		{
+			return BadRequest( ex.Message );
+		}
+	}
+}
--- Hexagonal.Api/Controllers/QuoteController.cs
+++ Hexagonal.Api/Controllers/QuoteController.cs
@@ -1,36 +1,27 @@
 ﻿using LaPinguinera.Quotes.Application.DTOs;
 using LaPinguinera.Quotes.Application.Generic;
 using LaPinguinera.Quotes.Domain.Model.Quote.Commands;
-using LaPinguinera.Quotes.Domain.Model.Quote.Values.Root;
-using LaPinguinera.Quotes.Infrastructure.Api.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using System.Reactive.Linq;
 
 namespace LaPinguinera.Quotes.Infrastructure.Api.Controllers;
 
 [ApiController]
-[Route( "api/v1/quote" )]
+[Route( "api/v1/secret" )]
 public class QuoteController : ControllerBase
 {
-	[HttpPost( "individual" )]
-	public async Task<IActionResult> CalculateIndividual
+	[HttpPost]
+	public async Task<IActionResult> CreateQuote
 	(
-		[FromBody] CalculateIndividualReqDTO request,
-		[FromServices] ICommandUseCase<CalculateIndividualCommand, QuoteId, CalculateIndividualResDTO> useCaseCommand
+		[FromServices] IInitialCommandUseCase<CreateQuoteCommand, CreateQuoteResDTO> useCaseCommand,
+		CreateQuoteCommand command
 	)
 	{
 		try
 		{
-			CalculateIndividualCommand command = new(
-				request.AggregateId,
-				request.Title,
-				request.Author,
-				request.Price,
-				request.Type
-			);
-			IObservable<CalculateIndividualCommand> subject = Observable.Return( command );
-			IObservable<CalculateIndividualResDTO> resultObservable = useCaseCommand.Execute( subject );
-			CalculateIndividualResDTO result = await resultObservable.FirstAsync();
+			IObservable<CreateQuoteCommand> subject = Observable.Return( command );
+			IObservable<CreateQuoteResDTO> resultObservable = useCaseCommand.Execute( subject );
+			CreateQuoteResDTO result = await resultObservable.FirstAsync();
 
 			return Ok( result );
 		}
@@ -40,84 +31,16 @@ public class QuoteController : ControllerBase
 		}
 	}
 
-	[HttpPost( "list" )]
-	public async Task<IActionResult> CalculateList
+	[HttpGet]
+	public async Task<IActionResult> GetAggregate
 	(
-		[FromBody] CalculateListReqDTO request,
-		[FromServices] ICommandUseCase<CalculateListCommand, QuoteId, CalculateListResDTO> useCaseCommand
-	)
+				[FromServices] IGetAggregateUseCase<GetAggregateResDTO> useCase
+			)
 	{
 		try
 		{
-			List<(string bookId, int quantity)> bookIdWithQuantity = request.Books
-				.Select( b => (bookId: b.BookId, quantity: b.Quantity) )
-				.ToList();
-			CalculateListCommand command = new(
-				request.AggregateId,
-				bookIdWithQuantity,
-				request.CustomerRegisterDate
-			);
-			IObservable<CalculateListCommand> subject = Observable.Return( command );
-			IObservable<CalculateListResDTO> resultObservable = useCaseCommand.Execute( subject );
-			CalculateListResDTO result = await resultObservable.FirstAsync();
-
-			return Ok( result );
-		}
-		catch (Exception ex)
-		{
-			return BadRequest( ex.Message );
-		}
-	}
-
-	[HttpPost( "budget" )]
-	public async Task<IActionResult> CalculateBudget
-	(
-		[FromBody] CalculateBudgetReqDTO request,
-		[FromServices] ICommandUseCase<CalculateBudgetCommand, QuoteId, CalculateBudgetResDTO> useCaseCommand
-	)
-	{
-		try
-		{
-			CalculateBudgetCommand command = new(
-				request.AggregateId,
-				request.BookIds,
-				request.Budget,
-				request.CustomerRegisterDate
-			);
-			IObservable<CalculateBudgetCommand> subject = Observable.Return( command );
-			IObservable<CalculateBudgetResDTO> resultObservable = useCaseCommand.Execute( subject );
-			CalculateBudgetResDTO result = await resultObservable.FirstAsync();
-
-			return Ok( result );
-		}
-		catch (Exception ex)
-		{
-			return BadRequest( ex.Message );
-		}
-	}
-
-	[HttpPost( "group" )]
-	public async Task<IActionResult> CalculateGroup
-(
-	[FromBody] CalculateGroupReqDTO request,
-	[FromServices] ICommandUseCase<CalculateGroupCommand, QuoteId, CalculateGroupResDTO> useCaseCommand
-)
-	{
-		try
-		{
-			List<List<(string bookId, int quantity)>> bookIdWithQuantityGroup = request.Group
-				.Select( g => g
-					.Select( b => (bookId: b.BookId, quantity: b.Quantity) )
-					.ToList() )
-				.ToList();
-			CalculateGroupCommand command = new(
-				request.AggregateId,
-				bookIdWithQuantityGroup,
-				request.CustomerRegisterDate
-			);
-			IObservable<CalculateGroupCommand> subject = Observable.Return( command );
-			IObservable<CalculateGroupResDTO> resultObservable = useCaseCommand.Execute( subject );
-			CalculateGroupResDTO result = await resultObservable.FirstAsync();
+			IObservable<GetAggregateResDTO> resultObservable = useCase.Execute();
+			GetAggregateResDTO result = await resultObservable.FirstAsync();
 
 			return Ok( result );
 		}
